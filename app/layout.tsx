57108d7a3f65d02b4a67f2d40e789df71185cc67
unknown <abni4250@gmail.com>
2026-04-22 00:37:23 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,7 +28,29 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Ad Scripts */}
+        <Script src="https://pl29213586.profitablecpmratenetwork.com/6d/9b/fa/6d9bfa436bd5f71cd4026a6da3e3439f.js" strategy="lazyOnload" />
+        <Script src="https://pl29213587.profitablecpmratenetwork.com/fa9f8bf0ea36a4796514b86c6f0f5fe2/invoke.js" strategy="lazyOnload" />
+        <Script src="https://pl29213589.profitablecpmratenetwork.com/fb/08/0c/fb080c513d25b3cca956d2b0dda2d17d.js" strategy="lazyOnload" />
+        
+        {/* Ad configuration for highperformanceformat */}
+        <Script id="at-options" strategy="lazyOnload">
+          {`
+            atOptions = {
+              'key' : 'c5192cb318a481a390982c0fe68c171e',
+              'format' : 'iframe',
+              'height' : 300,
+              'width' : 160,
+              'params' : {}
+            };
+          `}
+        </Script>
+        <Script src="https://www.highperformanceformat.com/c5192cb318a481a390982c0fe68c171e/invoke.js" strategy="lazyOnload" />
+        
         {children}
+        
+        {/* Ad Containers */}
+        <div id="container-fa9f8bf0ea36a4796514b86c6f0f5fe2" />
       </body>
     </html>
   );
